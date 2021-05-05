dbdbf8c60bc29ff7d35ddc24f0debf458061f3fe
Zac Pez <Zachary.Pez@nttdata.com>
2021-05-05 14:22:28 -0700

    add: vscode spring initializer generate spring application

---
+++ src/main/java/com/zacpez/sampledocker/SampleDockerApplication.java
@@ -0,0 +1,13 @@
+package com.zacpez.sampledocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zacpez/sampledocker/SampleDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zacpez.sampledocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
